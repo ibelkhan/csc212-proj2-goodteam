ec85fc41865e401fff89b2f35575a544fd045f1f
mitchellg <mitchellgordon0@gmail.com>
2013-10-12 01:04:34 +0000

    made db entry work

--- addSeminar.php
+++ addSeminar.php
@@ -7,18 +7,36 @@ $food = $_REQUEST['food'];
 $department = $_REQUEST['department'];
 $time = $_REQUEST['time'];
 
+$db = new PDO('mysql:host=localhost;dbname=project2', 'root', 'password');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$db = mysqli_connect("localhost","root","password","project2");
-$sql = "INSERT INTO seminars (date, time, speaker, description, department, food, location) VALUES(STR_TO_DATE($date,'%d/%m/%Y %h:%i:%s'), $speaker, $description, $department, $food, $location)";
-$result = $db->query($sql);
-
-if (!$result) {
-   printf("%s\n", $db->error);
-   exit();
+$stmt = $db->prepare("INSERT INTO seminars(date, time, speaker, description, department, food, location) VALUES(STR_TO_DATE(:date, '%m/%d/%Y'),:time,:speaker,:description,:department,:food,:location)");
+$test = array(':date' => $date, ':time' => '5:50pm', ':speaker' => 'mitchell', ':description' => 'fgdgfds', ':department' => 'fdsafd', ':food'=>'fdsfdsa', ':location'=>'fdsafdsa');
+try {
+    $stmt->execute($test);
+}
+catch (PDOException $e) {
+    echo $e->getMessage();
 }
 
+// $db = mysqli_connect("localhost","root","password","project2");
+// $sql = $db->prepare("INSERT INTO seminars (date, time, speaker, description, department, food, location) VALUES(?,?,?,?,?,?,?)");
+// $sql->bind_param('2013-10-20', $time, $speaker, $description, $department, $food, $location);
+// $sql->bind_param('2013-10-20', '5:50pm', 'mitchell', 'gfgf', 'fdsafd', 'fdsaf', 'fdsafd');
+// $sql->execute();
 
-echo $date;
+// echo $date;
+
+// if (!$result) {
+//    echo "start";
+//    printf("%s\n", $db->error);
+//    exit();
+// }
+
+echo "worked";
+
+
+// echo $date;
 
 
 ?>
