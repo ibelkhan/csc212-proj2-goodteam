ad1c437248521b351e3798493209d05b713e52d7
mitchellg <mitchellgordon0@gmail.com>
2013-11-15 16:32:52 +0000

    update authoring page

---
+++ GroupMaker/studentPg.php
@@ -0,0 +1,143 @@
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8"></meta>
+            <title>
+
+                Student Entry
+
+            </title>
+            <meta content="width=device-width, initial-scale=1.0" name="viewport"></meta>
+            <meta content="" name="description"></meta>
+            <meta content="" name="author"></meta>
+            <!--
+
+             CSS 
+
+            -->
+            <link rel="stylesheet" href="css/bootstrap.css"></link>
+            <link rel="stylesheet" href="css/footer.css"></link>
+            <link rel="stylesheet" href="css/bootstrap-responsive.css"></link>
+            <!--
+
+             HTML5 shim, for IE6-8 support of HTML5 elements 
+
+            -->
+            <!--
+
+            [if lt IE 9]>
+                  <script src="../assets/js/html5shiv.js"></script>
+                <![endif]
+
+            -->
+            <!--
+
+             Fav and touch icons 
+
+            -->
+            <link href="../assets/ico/apple-touch-icon-144-precomposed.png" sizes="144x144" rel="apple-touch-icon-precomposed"></link>
+            <link href="../assets/ico/apple-touch-icon-114-precomposed.png" sizes="114x114" rel="apple-touch-icon-precomposed"></link>
+            <link href="../assets/ico/apple-touch-icon-72-precomposed.png" sizes="72x72" rel="apple-touch-icon-precomposed"></link>
+            <link href="../assets/ico/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon-precomposed"></link>
+            <link href="../assets/ico/favicon.png" rel="shortcut icon"></link>
+        </head>
+        <body>
+            <!--
+
+             Part 1: Wrap all page content here 
+
+            -->
+            <div id="wrap">
+                <!--
+
+                 Begin page content 
+
+                -->
+                <div class="container">
+                    <div class="page-header">
+                        <h1>
+
+                            Class Name
+
+                        </h1>
+                    </div>
+			<form name="input" action="" method="post"> 
+			<p class="lead">
+
+                        Select your name from the list:
+
+                    </p>
+
+			<p>
+				<select>
+					<option value="1">Alex Alexander</option>
+					<option value="2">Dave Davidson</option>
+					<option value="3">Jimmy John</option>
+					<option value="4">Ritchie Rich</option>
+					<option value="5">Sam Samuels</option>
+				</select>
+			</p>
+			
+			<p class="lead">
+
+                        Select the attributes that best describe you:
+
+                    </p>
+			
+			<p>
+
+			<input type="checkbox" name="attribute" value="webcode"> Web Programming<br>
+			<input type="checkbox" name="attribute" value="javacode"> Java/C/Python Programming<br>			
+			<input type="checkbox" name="attribute" value="present"> Presentation Skills<br>
+			<input type="checkbox" name="attribute" value="organization"> Organizational Skills<br>
+			<input type="checkbox" name="attribute" value="useless"> No Skills<br>
+
+			</p>
+			
+			<p class="lead">
+				
+				<input type="submit" value="Submit">
+
+			</p>
+
+			</form>
+                
+                </div>
+                <div id="push"></div>
+            </div>
+            <div id="footer">
+                <div class="container">
+                    <p class="muted credit">
+
+                  Group Maker 2013. csc212-groupmaker@googlegroups.com
+
+                    </p>
+                </div>
+            </div>
+            <!--
+
+             Le javascript
+                ================================================== 
+
+            -->
+            <!--
+
+             Placed at the end of the document so the pages load faster 
+
+            -->
+            <script src="../assets/js/jquery.js"></script>
+            <script src="../assets/js/bootstrap-transition.js"></script>
+            <script src="../assets/js/bootstrap-alert.js"></script>
+            <script src="../assets/js/bootstrap-modal.js"></script>
+            <script src="../assets/js/bootstrap-dropdown.js"></script>
+            <script src="../assets/js/bootstrap-scrollspy.js"></script>
+            <script src="../assets/js/bootstrap-tab.js"></script>
+            <script src="../assets/js/bootstrap-tooltip.js"></script>
+            <script src="../assets/js/bootstrap-popover.js"></script>
+            <script src="../assets/js/bootstrap-button.js"></script>
+            <script src="../assets/js/bootstrap-collapse.js"></script>
+            <script src="../assets/js/bootstrap-carousel.js"></script>
+            <script src="../assets/js/bootstrap-typeahead.js"></script>
+        </body>
+    </html>
+
